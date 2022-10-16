ef47e60340d9b6ff17d3c6c3470cf69c8084a63c
Colby Albers <99503345+colby-albers@users.noreply.github.com>
2022-10-16 11:20:30 -0400

    Add files via upload

---
+++ user_with_bank_accounts.py
@@ -0,0 +1,53 @@
+class BankAccount:
+    accounts = []
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self,amount):
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        return f"Balance:{self.balance}"
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+class User:
+
+    def __init__(self,name):
+        self.name = name
+        self.account = BankAccount(.05, 1000)
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: {self.account.display_account_info()}")
+        return self
+
+    def transfer_money(self,amount,user):
+        self.amount -= amount
+        user.amount += amount
+        self.display_user_balance()
+        user.display_user_balance()
+        return self
+
+colby = User("Colby")
+
+colby.account.deposit(100)
+colby.display_user_balance()
